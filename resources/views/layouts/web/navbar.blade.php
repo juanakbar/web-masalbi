b20ff8ffbbefefac896f0ad18bd1f0b732382d6a
juanakbar <94147156+juanakbar@users.noreply.github.com>
2022-04-10 22:21:04 +0700

    update

--- resources/views/layouts/web/navbar.blade.php
+++ resources/views/layouts/web/navbar.blade.php
@@ -11,26 +11,37 @@
     <title>MAS AL-BIDAYAH</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
-        <div class="container">
-            <div class="navbar-brand">
-                <img src="img/portal/logo.png" alt="" width="40" height="40" class="d-inline-block align-center" style="font-weight: 600">
-                 MAS AL-BIDAYAH
-            </div>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-      
-      @yield('content')
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white shadow fixed-top">
+    <div class="container">
+      <div class="navbar-brand">
+        <img src="img/portal/logo.png" alt="" width="40" height="40" class="d-inline-block align-center" style="font-weight: 600">
+        MAS AL-BIDAYAH
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link" href="/beranda"> Beranda </a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#" data-bs-toggle="dropdown"> Profile </a>
+            <ul class="dropdown-menu dropdown-menu-start fade-down">
+              <li><a class="dropdown-item" href="#"> Sejarah Madrasah</a></li>
+              <li><a class="dropdown-item" href="#"> Tentang Madrasah</a></li>
+              <li><a class="dropdown-item" href="#"> Tujuan Madrasah</a></li>
+              <li><a class="dropdown-item" href="#"> Target Madrasah</a></li>
+            </ul>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="#"> Profile </a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+    
+    @yield('content')
+  
     
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -2,7 +2,56 @@
 
 
 @section('content')
-<div class="container">
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/img/portal/no-image.png" alt="" class="bd-placeholder-img" width="100%">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Some representative placeholder content for the second slide of the carousel.</p>
+              <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="/img/website/maxresdefault.jpeg" alt="" class="bd-placeholder-img" width="100%">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Some representative placeholder content for the second slide of the carousel.</p>
+              <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="/img/portal/no-image.png" alt="" class="bd-placeholder-img" width="100%" >
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Some representative placeholder content for the second slide of the carousel.</p>
+              <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+
+{{-- <div class="container">
   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -17,7 +66,7 @@
           <p>Some representative placeholder content for the first slide.</p>
         </div>
       </div>
-      {{-- <div class="carousel-item">
+      <div class="carousel-item">
         <img src="img/portal/no-image.png" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>Second slide label</h5>
@@ -30,7 +79,7 @@
           <h5>Third slide label</h5>
           <p>Some representative placeholder content for the third slide.</p>
         </div>
-      </div> --}}
+      </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -41,12 +90,51 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
-  
-</div>
+</div> --}}
 
 <div class="container mt-5 ">
   <div class="row">
     <h1 class="judul">Berita</h1>
+  </div>
+  <div class="row mt-3">
+    <div class="col-sm-6 col-md-4 mt-2 mb-3">
+      <div class="card">
+        <img src="img/portal/no-image.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <hr class="hr-content">
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 mt-2 mb-3">
+      <div class="card">
+        <img src="img/portal/no-image.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <hr class="hr-content">
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 mt-2 mb-3">
+      <div class="card">
+        <img src="img/portal/no-image.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <hr class="hr-content">
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col-sm-12 pt-4">
+        <a href="#" class="btn btn-outline-primary"> Tampilkan Semua Berita </a>
+      </div>
+    </div>
   </div>
 </div>
         
