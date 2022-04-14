80c3e6667423e1f4436663ec271ac59bd9c4a6d9
juanakbar <94147156+juanakbar@users.noreply.github.com>
2022-04-15 00:52:48 +0700

    up

--- resources/views/website/Single.blade.php
+++ resources/views/website/Single.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <div class="container berita mt-4 mb-4 border rounded isi shadow">
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="mt-5 margin-auto">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="/beranda/berita">Berita</a></li>
+              <li class="breadcrumb-item active" aria-current="page">{{$berita-> judul}}</li>
+            </ol>
+          </nav>
         <h1 class="text-center mt-1 bt">{{$berita-> judul}}</h1>
         <img src="/img/website/maxresdefault.jpeg" class="img-fluid mt-3 rounded" alt=" {{$berita->judul}} ">
         <p class="text-muted mt-2"><i class="fa-regular fa-calendar"></i> {{$berita-> created_at}} </p>
