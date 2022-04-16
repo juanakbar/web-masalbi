589e8d0190ade88c3d96b5297dd6ff097476c7b4
juanakbar <94147156+juanakbar@users.noreply.github.com>
2022-04-17 05:23:10 +0700

    gas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BeritaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/beranda', function(){
-    return view( 'beranda');
-})->name('beranda');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+Route::get('/beranda', [BeritaController::class, 'index']);
+// Route::get('/beranda', [PrestasiController::class, 'index']);
+// Route::get('/beranda', [BeritaController::class, 'prestasi']);
+// Route::get('/berita/{berita:slug}', [HomeController::class, 'show']);
+
+
+// Route::get('/beranda', [PrestasiController::class, 'index']);
+// Route::get('/beranda/prestasi', [PrestasiController::class, 'prestasi']);
+// Route::get('/berita/{prestasi:slug}', [PrestasiController::class, 'show']);
