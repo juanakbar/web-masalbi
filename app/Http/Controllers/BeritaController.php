12c6766b4f3bc57a3513879f2651eae3159a9f64
juanakbar <94147156+juanakbar@users.noreply.github.com>
2022-04-17 05:17:14 +0700

    breeze

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\User;
 use App\Models\Berita;
 use Illuminate\Http\Request;
 
@@ -23,7 +24,7 @@ class BeritaController extends Controller
         return view('website.berita',
         [
             'beritas'=>Berita::all(),
-            "title"=> "Home",
+            "title" => "Home",
         ]);
     }
     
